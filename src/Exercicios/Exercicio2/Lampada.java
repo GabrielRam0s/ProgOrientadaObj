a72ae00518ee8d4a45bc3a161fa7041ddc0a92dc
Ramos <gabrielmcramos@hotmail.com>
2024-03-24 22:06:41 -0300

    Exercicio 2

--- src/Exercicios/Exercicio2/Lampada.java
+++ src/Exercicios/Exercicio2/Lampada.java
@@ -1,30 +1,28 @@
 package Exercicios.Exercicio2;
 
 public class Lampada {
-
     private Estado estado;
-    private int contadorAcendimentos;
+    private int qtdAcendimentos;
 
     public Lampada() {
-        this.estado = Estado.APAGADA;
-        this.contadorAcendimentos = 0;
+        this.estado = Estado.APAGADA; // Estado inicial
     }
 
     public void click() {
-        if (this.estado == Estado.APAGADA) {
-            this.estado = Estado.ACESA;
-            contadorAcendimentos++;
-        } else {
+        if (this.estado == Estado.ACESA) {
             this.estado = Estado.APAGADA;
+        } else {
+            this.estado = Estado.ACESA;
+            qtdAcendimentos++;
         }
     }
 
     public int qtdAcendimentos() {
-        return contadorAcendimentos;
+        return qtdAcendimentos;
     }
 
-    public String checaEstado() {
-        return "A lâmpada está " + this.estado.toString().toLowerCase() + ".";
+    public Estado checaEstado() {
+        return estado;
     }
 }
 
--- src/Exercicios/Exercicio2/Main.java
+++ src/Exercicios/Exercicio2/Main.java
@@ -3,15 +3,12 @@ package Exercicios.Exercicio2;
 public class Main {
     public static void main(String[] args) {
         Lampada lampada = new Lampada();
-
-        System.out.println(lampada.checaEstado());
+        
+        System.out.println("Estado inicial: " + lampada.checaEstado());
         lampada.click();
-        System.out.println(lampada.checaEstado());
+        System.out.println("Estado após 1º click: " + lampada.checaEstado());
         lampada.click();
-        System.out.println(lampada.checaEstado());
-        lampada.click();
-        System.out.println(lampada.checaEstado());
-
-        System.out.println("A lâmpada foi acesa " + lampada.qtdAcendimentos() + " vezes.");
+        System.out.println("Estado após 2º click: " + lampada.checaEstado());
+        System.out.println("Quantidade de acendimentos: " + lampada.qtdAcendimentos());
     }
 }
