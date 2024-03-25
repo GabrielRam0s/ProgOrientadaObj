57c1aa5ee361174e6a4dc3b090b5b95a1c7706c0
Ramos <gabrielmcramos@hotmail.com>
2024-03-24 22:46:44 -0300

    Correcao 2 Exercicio 4

--- src/Exercicios/Exercicio4/Cliente.java
+++ src/Exercicios/Exercicio4/Cliente.java
@@ -1,7 +1,7 @@
 package Exercicios.Exercicio4;
 
 public class Cliente {
-    // Atributos
+
     private String nome;
     private double saldo;
     private double limite;
@@ -13,34 +13,30 @@ public class Cliente {
         this.limite = limite;
     }
 
-    // Método para sacar uma quantia do saldo
     public boolean sacar(double quantia) {
         double saldoTotal = saldo + limite;
         if (quantia <= saldoTotal) {
             if (quantia <= saldo) {
-                saldo -= quantia; // Atualiza o saldo subtraindo a quantia sacada
+                saldo -= quantia;
             } else {
                 double restante = quantia - saldo;
-                saldo = 0; // Saldo fica zerado
-                limite -= restante; // Desconta o restante do limite
+                saldo = 0;
+                limite -= restante;
             }
-            return true; // Saque realizado com sucesso
+            return true;
         } else {
-            return false; // Saque não realizado, valor excede o saldo + limite
+            return false;
         }
     }
 
-    // Método para depositar uma quantia no saldo
     public void depositar(double quantia) {
         saldo += quantia;
     }
 
-    // Método para checar o saldo (saldo + limite)
     public double checarSaldo() {
         return saldo + limite;
     }
 
-    // Método para obter o nome do cliente
     public String obterNome() {
         return nome;
     }
