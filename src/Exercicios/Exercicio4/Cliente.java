56e5912b99b2e4be8eda855062166c7dede56993
Jose Leandro Santos Cruz <leandros_cruz@outlook.com>
2024-03-25 00:53:11 -0300

    Atualização

--- src/Exercicios/Exercicio4/Cliente.java
+++ src/Exercicios/Exercicio4/Cliente.java
@@ -12,25 +12,30 @@ public class Cliente {
         this.limite = limite;
     }
 
-    public boolean sacar(double quantia) {
-        double saldoTotal = saldo + limite;
-        if (quantia <= saldoTotal) {
+    public void sacar(double quantia, boolean mostrarMensagem) {
+        if (quantia <= checarSaldo()) {
             if (quantia <= saldo) {
                 saldo -= quantia;
             } else {
-                double restante = quantia - saldo;
+                limite -= quantia - saldo;
                 saldo = 0;
-                limite -= restante;
             }
-            return true;
-        } else {
-            return false;
+            if(mostrarMensagem){
+                System.out.println(obterNome()+", saque de R$" + quantia + " foi realizado com sucesso. Seu novo saldo: R$"+checarSaldo());
+            }
+        } else if (mostrarMensagem) {
+            System.out.println(obterNome()+ ", não foi possível realizar o saque de R$"+quantia+". Saldo insuficiente. Seu saldo atual é R$"+checarSaldo());
         }
     }
 
-    public void depositar(double quantia) {
+    public void depositar(double quantia, boolean mostrarMensagem) {
         saldo += quantia;
+        if(mostrarMensagem){
+            System.out.println(obterNome()+", depósito de R$" + quantia+ " foi realizado com sucesso. Seu novo saldo é: R$"+checarSaldo());
+        }
+            
     }
+
 
     public double checarSaldo() {
         return saldo + limite;
@@ -38,5 +43,12 @@ public class Cliente {
 
     public String obterNome() {
         return nome;
+    }
+
+    public void extrato(){
+        System.out.println("-----------------EXTRATO----------------");
+        System.out.println("NOME DO TITULAR: "+obterNome());
+        System.out.println("SALDO: R$"+checarSaldo());
+        System.out.println("----------------------------------------");
     }
 }
--- src/Exercicios/Exercicio4/Main.java
+++ src/Exercicios/Exercicio4/Main.java
@@ -2,24 +2,13 @@ package Exercicios.Exercicio4;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Gabriel Ramos", 1000.0, 500.0);
+        Cliente cliente1 = new Cliente("Gabriel Ramos", 1000.0, 500.0);
 
-        System.out.println("Cliente: " + cliente.obterNome());
-        System.out.println("Saldo inicial: " + cliente.checarSaldo());
+        cliente1.extrato();
+        cliente1.sacar(1600, true);
+        cliente1.sacar(1500, true);
 
-        if (!cliente.sacar(1600)) {
-            System.out.println("Não foi possível realizar o saque. Saldo insuficiente.");
-        }
-
-        if (cliente.sacar(1500)) {
-            System.out.println("Saque realizado com sucesso.");
-        } else {
-            System.out.println("Não foi possível realizar o saque.");
-        }
-
-        System.out.println("Saldo após saque: " + cliente.checarSaldo());
-
-        cliente.depositar(200);
-        System.out.println("Saldo após depósito: " + cliente.checarSaldo());
+        cliente1.depositar(200, true);
+        cliente1.extrato();
     }
 }
